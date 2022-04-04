c0780759c83c8e1e47d13c848ea49888af552355
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-03 20:39:41 -0500

    add click event listener for saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 var events = [];
 
+$(".saveBtn").each(function(index) {
+
+    $(this).on("click", function() {
+    var text = $('textarea').val();
+
+    localStorage.setItem("event description", text);
+});
+});
+
+var Events = function() {
+    $(".description").each(function(index) {
+        console.log()
+        });
+    };
