96bd25b31a35862e0a9e49de1a6750cf03ba1656
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-03 20:52:22 -0500

    add ids to textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 var events = [];
 
-$(".saveBtn").each(function(index) {
-
-    $(this).on("click", function() {
+$(".saveBtn").on("click", function() {
     var text = $('textarea').val();
 
     localStorage.setItem("event description", text);
 });
-});
 
 var Events = function() {
     $(".description").each(function(index) {
-        console.log()
         });
     };
