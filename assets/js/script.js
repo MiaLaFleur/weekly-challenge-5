01e4523ac2ef9b2c4569a6a15580ceac0906be2f
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-03 23:59:35 -0500

    finished loadTasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,20 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(time, desc);
 });
 
-var LoadEvents = function(time, desc) {
-    var savedEvents = JSON.parse(localStorage.getItem("time"));
-    console.log(savedEvents);
-
+var LoadEvents = function() {
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#1").val(localStorage.getItem("1"));
+    $("#2").val(localStorage.getItem("2"));
+    $("#3").val(localStorage.getItem("3"));
+    $("#4").val(localStorage.getItem("4"));
+    $("#5").val(localStorage.getItem("5"));
 }
 
 LoadEvents();
 
-var Events = function() {
-    $(".description").each(function(index) {
-        });
-    };
+var checkEventTime = function() {
+    
+}
