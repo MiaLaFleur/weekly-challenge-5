1818431523397c8538093e86a6d258a73f3f1ca5
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-03 21:07:44 -0500

    on click listener fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 var events = [];
 
 $(".saveBtn").on("click", function() {
-    var text = $('textarea').val();
 
-    localStorage.setItem("event description", text);
+    var time = $(this).siblings(".description").attr("id");
+    var desc = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, desc);
 });
+
+var LoadEvents = function() {
+    localStorage.getItem(time, desc);
+}
+
+LoadEvents();
 
 var Events = function() {
     $(".description").each(function(index) {
